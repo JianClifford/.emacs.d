894fb695cf9b8407394bd21ed01a4a1e8c57ad2f
Jian <jian.linlong@foxmail.com>
2023-09-11 21:03:22 +0800

    change font in init-theme.el

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -5,7 +5,14 @@
      (load-theme 'srcery t))
 ;;(load-theme 'solarized-dark t)
 
-
+(if (not (display-graphic-p))
+    (progn
+      ;; 在终端环境下加载的配置
+      (set-frame-font "Source Code Pro-12")
+      )
+  ;; 在图形界面下加载的配置
+  (set-frame-font "Source Code Pro-15")
+  )
 
 (provide 'init-themes)
 ;; init-themes.el ends here
